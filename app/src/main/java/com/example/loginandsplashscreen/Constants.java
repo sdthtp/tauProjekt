727b281bc91ebfa81bdbe22445209d47d25965d7
Muhammed Bozkus <mzbozkus@hotmail.com>
2019-04-09 11:58:05 +0300

    Update Constants.java

--- app/src/main/java/com/example/loginandsplashscreen/Constants.java
+++ app/src/main/java/com/example/loginandsplashscreen/Constants.java
@@ -3,5 +3,5 @@ package com.example.loginandsplashscreen;
 public class Constants {
     public static final String QUERY = "172.17.24.108";
     public static final int PORT = 8080;
-    //adsa
+
 }
