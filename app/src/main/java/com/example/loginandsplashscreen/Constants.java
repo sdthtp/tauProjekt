c575ef9757a068544a34d58ca6c7f34852f4eaeb
Sedat H <hatipsedat@gmail.com>
2019-04-10 15:35:30 +0300

    minor changes

--- app/src/main/java/com/example/loginandsplashscreen/Constants.java
+++ app/src/main/java/com/example/loginandsplashscreen/Constants.java
@@ -1,6 +1,6 @@
 package com.example.loginandsplashscreen;
 
 public class Constants {
-    public static final String QUERY = "192.168.43.112";
+    public static final String QUERY = "192.168.137.1";
     public static final int PORT = 8080;
 }
