3b1940228a3a6878021a6ba5195293401f2b815b
deniz997 <36201330+deniz997@users.noreply.github.com>
2019-05-14 02:50:56 +0300

    Faster load of the user-info in header in sidebar

--- app/src/main/java/com/example/loginandsplashscreen/Constants.java
+++ app/src/main/java/com/example/loginandsplashscreen/Constants.java
@@ -1,7 +1,7 @@
 package com.example.loginandsplashscreen;
 
 public class Constants {
-    public static final String QUERY = "workshop-omercem.tk";
+    public static final String QUERY = "workshop-omercem.com";
     public static final int PORT = 8443;
 
 }
